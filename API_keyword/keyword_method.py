f51b2ab8628cbf73ac10b9d652ef830aeddc54cb
SmileAK-47 <125081306@qq.com>
2022-04-19 22:46:15 +0800

    1.完成ymal 2完成unittest第一阶段

--- API_keyword/keyword_method.py
+++ API_keyword/keyword_method.py
@@ -1,12 +1,33 @@
-#接口自动化关键字
+# 接口自动化关键字
 
 import requests
+import jsonpath
+import json
+
 
 class method(object):
-    #定义get
-    def get(self,url,params=None,headers=None):
-        return requests.get(url=url,params=params,headers=headers)
-    #定义post
-    def post(self,url,headers=None,data=None):
-        return requests.post(url=url,headers=headers,json=data)
+    # 定义get
+    def get(self, url, params=None, headers=None):
+        return requests.get(url=url, params=params, headers=headers)
 
+    # 定义post
+    def post(self, url, headers=None, data=None):
+        return requests.post(url=url, headers=headers, json=data)
+
+    # 校验字段获取方法
+    def get_text(self, res, key):
+        if res is not None:
+            try:
+                # 将res文本转换为json 通过jsonpath解析获取指定值
+                text = json.loads(res)
+                value = jsonpath.jsonpath(text, '..{0}', format(key))
+                # json获取值为list类型，如果获取失败为False
+                if value:
+                    # 将list 转为string格式
+                    if len(value) == 1:
+                        return value[0]
+                return value
+            except Exception as e:
+                return e
+        else:
+            return None
--- cases/api_cases_1.py
+++ cases/api_cases_1.py
@@ -2,8 +2,7 @@ import configparser
 import unittest
 from API_keyword.keyword_method import method
 from ddt import ddt, file_data
-import yaml
-
+# import pyyaml
 
 # 创建一个test框架
 
@@ -19,13 +18,15 @@ class ApiCase(unittest.TestCase):
         conf.read('../config/config.ini')
         dz = method()
         # D:\my_code\API\config\config.ini
-        url = conf.get('DEFAULT', 'url') + kwargs["path"]
+        url = conf.get('DEFAULT', 'url') + kwargs['path']
         # print(url)
         # 接口测试
-        res = dz.post(url=url, headers=kwargs["headers"], data=kwargs["data"])
+        res = dz.post(url=url, headers=kwargs['headers'], data=kwargs['data'])
         # aa = res.json()
         print(res.text)
-
+        value = dz.get_text(res.text,'code')
+        print(value)
+        self.assertEqual(first='200',second=value,msg='失败')
 
 if __name__ == '__main__':
     unittest.main()
