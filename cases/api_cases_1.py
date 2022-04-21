a0acd00f6ed9aa2e4cbc66a52774d6df021e9b18
SmileAK-47 <125081306@qq.com>
2022-04-21 23:28:56 +0800

    1.未完成unittest 完成 2.在tt文件里查看

--- cases/api_cases_1.py
+++ cases/api_cases_1.py
@@ -9,7 +9,7 @@ from ddt import ddt, file_data
 # 创建一个test框架
 
 @ddt
-class ApiCase(unittest.TestCase):
+class apiCase(unittest.TestCase):
 
    #公共部分 初始化，
     @classmethod
@@ -20,8 +20,6 @@ class ApiCase(unittest.TestCase):
         cls.url = conf.get('DEFAULT', 'url')
         cls.dz = method()
 
-    def setUp(self) -> None:
-        pass
         # 测试用例
 
     @file_data('../API_data/login.yaml')
@@ -35,22 +33,29 @@ class ApiCase(unittest.TestCase):
 
         token = self.dz.get_text(res.text, 'access_token')
         # token =dz.get_text(res.text,'')
-        # print(token)
+        print(token)
         # self.assertEqual(first=kwargskwargs['text'],second=value,msg='失败')
         # self.assertEqual(first=200,second=code['code'],msg='失败')
 
-    @file_data('../API_data/login.yaml')
+    @file_data('../API_data/member.yaml')
     def test_2_api_member(self,**kwargs):
         url = self.url + kwargs['path']
         # print(url)
         # 接口测试
-        res = self.dz.post(url=url, headers=kwargs['headers'], data=kwargs['data'])
+        headers = kwargs['headers']
 
-        token = self.dz.get_text(res.text, 'access_token')
+        headers['Authorization'] = 'Bear'+self.token
+        print(headers['Authorization'])
+        res = self.dz.get(url=url, headers=headers)
+
+        # token = self.dz.get_text(res.text, 'access_token')
         # token =dz.get_text(res.text,'')
-        print(token)
+        print(res.text)
     #     print("111")
     #     print(self.token)
 
+
+    def setUp(self) -> None:
+        pass
 if __name__ == '__main__':
     unittest.main()
--- test-git/tt.py
+++ test-git/tt.py
@@ -1,11 +1,25 @@
-def test(a, *args, **kwargs):
-    print(a)
-    # print b
-    # print c
-    print(args)
+# def test(a, *args, **kwargs):
+#     print(a)
+#     # print b
+#     # print c
+#     print(args)
+#
+#     print(kwargs)
+#
+#
+#
+# test(1, 2, 3, d='4', e=5)
+import unittest
 
-    print(kwargs)
+
+class api(unittest.TestCase):
+    def test_1_a(self):
+        print("111")
+    def test_2_b(self):
+        print("222")
+    def test_3_c(self):
+        print("333")
 
 
-
-test(1, 2, 3, d='4', e=5)
+if __name__ == '__main__':
+    unittest.main()
